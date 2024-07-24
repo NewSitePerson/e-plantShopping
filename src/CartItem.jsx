47909e88390571c6896d5a6fd7062c5832f90d58
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-07-24 01:32:27 -0400

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,11 +9,12 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+
+    item.cost;
   };
 
   const handleContinueShopping = (e) => {
-   
+   alert('Functionality to be added for future reference');
   };
 
 
